57176cc232e081dd228f7096959ab7b18c9d06ca
Liz Bright <elizabeth.anne.bright@gmail.com>
2018-02-16 18:16:10 -0500

    added css file and revised text and added other image

--- sketch.js
+++ sketch.js
@@ -32,7 +32,7 @@ let r, g, b;
 
 function setup() {
     let canvas = createCanvas(1024, 768);
-    canvas.position(25, 750);
+    canvas.position(15, 250);
     canvas.parent('container');
     background(150);
     angleMode(DEGREES);
